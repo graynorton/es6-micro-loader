44f7d22e51beff8fd122b64983e2db8be5fcb691
Gray Norton <graynorton@google.com>
2016-06-09 18:48:12 -0700

    Promisify

--- dist/system-polyfill.js
+++ dist/system-polyfill.js
@@ -1,17 +1,118 @@
 (function(exports) {
+  // Begin Object.assign polyfill
+  // from https://github.com/sindresorhus/object-assign
 
-'use strict';
+  'use strict';
+  /* eslint-disable no-unused-vars */
+  var hasOwnProperty = Object.prototype.hasOwnProperty;
+  var propIsEnumerable = Object.prototype.propertyIsEnumerable;
+
+  function toObject(val) {
+  	if (val === null || val === undefined) {
+  		throw new TypeError('Object.assign cannot be called with null or undefined');
+  	}
+
+  	return Object(val);
+  }
+
+  function shouldUseNative() {
+  	try {
+  		if (!Object.assign) {
+  			return false;
+  		}
+
+  		// Detect buggy property enumeration order in older V8 versions.
+
+  		// https://bugs.chromium.org/p/v8/issues/detail?id=4118
+  		var test1 = 'abc';  // eslint-disable-line
+  		test1[5] = 'de';
+  		if (Object.getOwnPropertyNames(test1)[0] === '5') {
+  			return false;
+  		}
+
+  		// https://bugs.chromium.org/p/v8/issues/detail?id=3056
+  		var test2 = {};
+  		for (var i = 0; i < 10; i++) {
+  			test2['_' + String.fromCharCode(i)] = i;
+  		}
+  		var order2 = Object.getOwnPropertyNames(test2).map(function (n) {
+  			return test2[n];
+  		});
+  		if (order2.join('') !== '0123456789') {
+  			return false;
+  		}
+
+  		// https://bugs.chromium.org/p/v8/issues/detail?id=3056
+  		var test3 = {};
+  		'abcdefghijklmnopqrst'.split('').forEach(function (letter) {
+  			test3[letter] = letter;
+  		});
+  		if (Object.keys(Object.assign({}, test3)).join('') !==
+  				'abcdefghijklmnopqrst') {
+  			return false;
+  		}
+
+  		return true;
+  	} catch (e) {
+  		// We don't expect any of the above to throw, but better to be safe.
+  		return false;
+  	}
+  }
+
+  var assign = shouldUseNative() ? Object.assign : function (target, source) {
+  	var from;
+  	var to = toObject(target);
+  	var symbols;
+
+  	for (var s = 1; s < arguments.length; s++) {
+  		from = Object(arguments[s]);
+
+  		for (var key in from) {
+  			if (hasOwnProperty.call(from, key)) {
+  				to[key] = from[key];
+  			}
+  		}
+
+  		if (Object.getOwnPropertySymbols) {
+  			symbols = Object.getOwnPropertySymbols(from);
+  			for (var i = 0; i < symbols.length; i++) {
+  				if (propIsEnumerable.call(from, symbols[i])) {
+  					to[symbols[i]] = from[symbols[i]];
+  				}
+  			}
+  		}
+  	}
+
+  	return to;
+  };
+
+// End Object.assign polyfill
 
 var headEl = document.getElementsByTagName('head')[0],
     ie = /MSIE/.test(navigator.userAgent);
 
-function mapPath(path) {
-  for (var pre in System.map) {
+function mapPath(path, packagePath) {
+  var pkg = packagePath && systemConfig.packages[packagePath];
+
+  return (pkg && pkg.map && _mapPath(path, pkg.map)) ||
+    _mapPath(path, systemConfig.map) ||
+    path;
+}
+
+function _mapPath(path, map) {
+  return path && map && (_fastMapPath(path, map) || _fullMapPath(path, map));
+}
+
+function _fastMapPath(path, map) {
+  return map[path];
+}
+
+function _fullMapPath(path, map) {
+  for (var pre in map) {
     if (path.indexOf(pre) === 0) {
-      return System.map[pre] + path.substring(pre.length);
+      return map[pre] + path.substring(pre.length);
     }
   }
-  return path;
 }
 
 function addSuffix(fileName) {
@@ -21,12 +122,12 @@ function addSuffix(fileName) {
 }
 
 function addBase(path) {
-  return (System.baseURL || '/') + path;
+  return (systemConfig.baseURL || '/') + path;
 }
 
 function getMain(path) {
   var pkg, main;
-  if ((pkg = System.packages[path])) {
+  if ((pkg = systemConfig.packages[path])) {
     if ((main = pkg.main)) {
       return path + '/' + main;
     }
@@ -59,24 +160,26 @@ var internalRegistry = Object.create(null);
 var externalRegistry = Object.create(null);
 var pendingLoads = Object.create(null);
 var pendingImports = Object.create(null);
+var systemConfig = Object.create(null);
 var anonymousEntry;
 
-function ensuredExecute(name) {
-    var mod = internalRegistry[name];
-    if (mod && !seen[name]) {
-        seen[name] = true;
-        // one time operation to execute the module body
-        mod.execute();
-    }
-    return mod && mod.proxy;
-}
+// function ensuredExecute(name) {
+//     var mod = internalRegistry[name];
+//     if (mod && !seen[name]) {
+//         seen[name] = true;
+//         // one time operation to execute the module body
+//         mod.execute();
+//     }
+//     return mod && mod.proxy;
+// }
 
 function set(name, values) {
-    externalRegistry[name] = values;
+    externalRegistry[name] = {values: values};
 }
 
 function get(name) {
-    return externalRegistry[name] || ensuredExecute(name);
+  return (externalRegistry[name] && externalRegistry[name].values) ||
+    (internalRegistry[name] && internalRegistry[name].values);
 }
 
 function has(name) {
@@ -85,6 +188,14 @@ function has(name) {
 
 function normalizeNameAndGet(name) {
   return get(normalizeName(name));
+}
+
+function config(inCfg) {
+  ['map', 'packages', 'paths', 'browserConfig'].forEach(function(prop) {
+    systemConfig[prop] = systemConfig[prop] || {};
+    assign(systemConfig[prop], inCfg[prop]);
+  });
+  if (inCfg.baseURL) systemConfig.baseURL = inCfg.baseURL;
 }
 
 function createScriptNode(src, callback) {
@@ -109,6 +220,8 @@ function createScriptNode(src, callback) {
 }
 
 function load(name) {
+    if (pendingLoads[name]) return pendingLoads[name];
+
     pendingLoads[name] = new Promise(function(resolve, reject) {
         createScriptNode(addBase(name), function(err) {
             if (anonymousEntry) {
@@ -121,38 +234,49 @@ function load(name) {
                 return;
             }
             Promise.all(mod.deps.map(function (dep) {
-                if (externalRegistry[dep] || internalRegistry[dep]) {
-                    return Promise.resolve();
-                }
-                if (pendingLoads[dep]) {
-                  return pendingLoads[dep];
-                }
-                pendingLoads[dep] = load(dep);
-                return pendingLoads[dep];
-            })).then(resolve, reject);
+                // if (externalRegistry[dep] || internalRegistry[dep]) {
+                //     return Promise.resolve();
+                // }
+                return load(dep);
+            })).then(function() {
+              resolve(mod);
+            });
         });
     });
     return pendingLoads[name];
 }
 
+function __import(name, skipNormalization) {
+    var nName = skipNormalization ? name : normalizeName(name, []);
+    if (pendingImports[nName]) {
+      return pendingImports[nName];
+    }
+    pendingImports[nName] = new Promise(function(resolve, reject) {
+        var ext = externalRegistry[nName];
+        if (ext) resolve(ext);
+
+        var int = internalRegistry[nName];
+        if (int) resolve(int.execute());
+
+        return load(nName).then(function(loaded) {
+            resolve(loaded.execute());
+        });
+    });
+    return pendingImports[nName];
+}
+
+function _import(name) {
+  return __import(name).then(function(mod) {
+    return mod.values;
+  });
+}
 
 var System = {
     set: set,
     get: get,
     has: has,
-    import: function(name) {
-        if (pendingImports[name]) {
-          return pendingImports[name];
-        }
-        pendingImports[name] = new Promise(function(resolve, reject) {
-            var normalizedName = normalizeName(name, []);
-            var mod = get(normalizedName);
-            return mod ? resolve(mod) : load(name).then(function () {
-                resolve(get(normalizedName));
-            });
-        });
-        return pendingImports[name];
-    },
+    config: config,
+    import: _import,
     register: function(name, deps, wrapper) {
         if (Array.isArray(name)) {
             // anounymous module
@@ -180,20 +304,25 @@ var System = {
                 meta.setters[mod.deps.indexOf(moduleName)](moduleObj);
             },
             execute: function() {
-                mod.deps.map(function(dep) {
-                    var imports = externalRegistry[dep];
-                    if (imports) {
-                        mod.update(dep, imports);
+              return new Promise(function(resolve, reject) {
+                return Promise.all(mod.deps.map(function(depName) {
+                    var dep = externalRegistry[depName];
+                    if (dep) {
+                        mod.update(depName, dep.values);
+                        return Promise.resolve();
                     } else {
-                        imports = get(dep) && internalRegistry[dep].values; // optimization to pass plain values instead of bindings
-                        if (imports) {
-                            internalRegistry[dep].dependants.push(name);
-                            mod.update(dep, imports);
-                        }
+                        return __import(depName).then(function(dep) {
+                          dep.dependants.push(name);
+                          mod.update(depName, dep.values);
+                          return Promise.resolve();
+                        });
                     }
+                })).then(function() {
+                  meta.execute();
+                  resolve(mod);
                 });
-                meta.execute();
-            }
+            });
+          }
         };
         // collecting execute() and setters[]
         meta = wrapper(function(identifier, value) {
@@ -226,7 +355,7 @@ var System = {
             return value;
         });
     },
-    registerDynamic: function (name, deps, executingRequire, declare) {
+    registerDynamic: function(name, deps, executingRequire, declare) {
       if (Array.isArray(name)) {
           // anounymous module
           anonymousEntry = [];
@@ -260,6 +389,7 @@ var System = {
               declare(normalizeDepNameAndGet, null, mod);
               mod.proxy = {default: mod.exports};
               mod.values = mod.proxy;
+              return Promise.resolve(mod);
               // mod.lock = true; // locking down the updates on the module to avoid infinite loop
               // mod.dependants.forEach(function(moduleName) {
               //     if (internalRegistry[moduleName] && !internalRegistry[moduleName].lock) {
@@ -274,5 +404,11 @@ var System = {
 
 // exporting the System object
 exports.System = System;
+exports.SystemJS = System;
+
+// temp
+System._config = systemConfig;
+System._pendingLoads = pendingLoads;
+System._pendingImports = pendingImports;
 
 })(window);
